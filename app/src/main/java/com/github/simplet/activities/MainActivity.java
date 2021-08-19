ac29b6a70c51aa17d345318f6e29b3bcf8cfce5d
kingcobra2468 <eriktyryshkin@gmail.com>
2021-08-18 22:52:01 -0700

    added skeleton for refresh loop and setup security config

--- app/src/main/java/com/github/simplet/activities/MainActivity.java
+++ app/src/main/java/com/github/simplet/activities/MainActivity.java
@@ -2,11 +2,14 @@ package com.github.simplet.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -21,17 +24,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
     //private LocalStorage localStorage;
-    private List<RpistNode> mRpistList = new ArrayList<>(Arrays.asList(
+    private final List<RpistNode> mRpistList = new ArrayList<>(Arrays.asList(
             new RpistNode(70, TemperatureScale.CELSIUS),
             new RpistNode(20, TemperatureScale.CELSIUS),
             new RpistNode(40, TemperatureScale.FAHRENHEIT)
     ));
     private RecyclerView mRecyclerView;
     private RpistAdapter mRpistAdapter;
-    // private AsyncTask temp_refresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,32 +49,46 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView = (RecyclerView) findViewById(R.id.rpist_recycle_view);
         mRpistAdapter = new RpistAdapter(this, mRpistList);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        RecyclerView.LayoutManager mLayoutManager =
+                new LinearLayoutManager(getApplicationContext());
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setAdapter(mRpistAdapter);
 
+        ExecutorService executor = Executors.newScheduledThreadPool(1);
+        HandlerThread ht = new HandlerThread("temperatureRefresh");
+        ht.start();
+
+        Handler handler = new Handler(ht.getLooper());
         RpistNodeClient client = new RpistNodeClient("http://10.0.1.184:8080/");
-        try {
-            client.connect("tester");
-            client.getCelsius(new RpistTempCallback() {
-                @Override
-                public void onSuccess(float temperature) {
-                    mRpistList.get(0).setTemperature(temperature);
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    client.connect("tester");
+                    client.getCelsius(new RpistTempCallback() {
+                        @Override
+                        public void onSuccess(float temperature) {
+                            mRpistList.get(0).setTemperature(temperature);
+                            mRpistAdapter.notifyDataSetChanged();
+                        }
+
+                        @Override
+                        public void onError(String code, String message) {
+                            mRpistList.get(0).setTemperature(-99);
+                            mRpistAdapter.notifyDataSetChanged();
+                        }
+                    });
+                } catch (IOException e) {
+                    Log.e("Network", e.toString());
+                    mRpistList.get(0).setTemperature(-199);
                     mRpistAdapter.notifyDataSetChanged();
                 }
 
-                @Override
-                public void onError(String code, String message) {
-                    mRpistList.get(0).setTemperature(-99);
-                    mRpistAdapter.notifyDataSetChanged();
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-            mRpistList.get(0).setTemperature(-199);
-            mRpistAdapter.notifyDataSetChanged();
-        }
+                handler.postDelayed(this, 2000);
+            }
+        });
     }
 
     @Override
@@ -85,12 +103,10 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case com.github.simplet.R.id.action_favorite:
                 Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
-                //myIntent.putExtra("key", value); //Optional parameters
                 MainActivity.this.startActivity(myIntent);
+
                 return true;
             default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
         }
     }
@@ -98,96 +114,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
-      //  temp_refresh = new MyTask().execute();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
-   //     temp_refresh.cancel(true);
     }
-    /*
-    private class MyTask extends AsyncTask<String, String, String> {
-        private APIRequest api_request = new APIRequest();
-        private String username, password, apiUrl, units, raspHash, apiMode;
-        private int refreshSec;
 
-        private void getLatestData() {
-            this.username = LocalStorage.getString(com.github.simplet.R.string.username, "");
-            this.password = LocalStorage.getString(com.github.simplet.R.string.password, "");
-            this.apiUrl = LocalStorage.getString(com.github.simplet.R.string.api_url,
-                    "http:127.0.1.1/");
-            this.units = LocalStorage.getString(com.github.simplet.R.string.unit, "celcius");
-            this.refreshSec = LocalStorage.getInt(com.github.simplet.R.integer.refresh, 5);
-            this.raspHash = LocalStorage.getString(com.github.simplet.R.string.rasp_pi_hash, "");
-            this.apiMode = LocalStorage.getString(com.github.simplet.R.string.api_mode, "Public");
-
-            if (this.api_request.getBaseUrl() != this.apiUrl) {
-                this.api_request.setUrl(this.apiUrl);
-            }
-
-            //Update each time or do a check. Check might be slower
-            this.api_request.setUsername(this.username);
-            this.api_request.setPassword(this.password);
-        }
-
-        @Override
-        protected void onPreExecute() {
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            String apiCall = "";
-            try {
-
-                MyTask.this.getLatestData();
-
-                while (!isCancelled()) {
-
-                    if (this.apiMode.equalsIgnoreCase("public")) {
-                        apiCall = "temperature/" + this.units.toLowerCase();
-                    } else if (this.apiMode.equalsIgnoreCase("private")) {
-                        apiCall = "temp/get-temperature/" + this.raspHash + "/" + this.units
-                                .toLowerCase();
-                    }
-
-                    try {
-                        JSONObject responseData = (JSONObject) this.api_request.sendRequest
-                                ("GET", apiCall, null, false);
-                        Double temperature = responseData.getDouble("temperature");
-                        String units = responseData.getString("unit");
-                        publishProgress(temperature.toString() + " " + units);
-                        Thread.sleep(refreshSec * 1000);
-                    } catch (Exception e) {
-
-                        if (e.getMessage().equalsIgnoreCase("")) {
-                            publishProgress("REQUEST ERROR");
-                        } else {
-                            publishProgress(e.getMessage());
-                        }
-                        Thread.sleep(5000);
-
-                    }
-
-                }
-            } catch (Exception e) {
-                publishProgress("ERROR");
-
-            }
-
-            return "done";
-        }
-
-        @Override
-        protected void onProgressUpdate(String... values) {
-            myTextView.setText(values[0]);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            myTextView.setText(result);
-        }
-    }*/
 }
